7871292b32e514791f6f2e717c21842f78cc0c46
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-10 08:30:36 +0900

    refactor posts CRUD views

--- wps_blog_practice/wpsblog/wpsblog/models.py
+++ wps_blog_practice/wpsblog/wpsblog/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse(
-                "post-detail",
+                "posts:detail",
                 kwargs={
                     "post_id": self.id,
                     }
