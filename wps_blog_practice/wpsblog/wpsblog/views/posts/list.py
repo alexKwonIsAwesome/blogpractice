43361cf8fb4f332b96f5121f4d5ca340bb73c89f
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-11 22:48:12 +0900

    update postst list view: refactor via public filter

--- wps_blog_practice/wpsblog/wpsblog/views/posts/list.py
+++ wps_blog_practice/wpsblog/wpsblog/views/posts/list.py
@@ -7,7 +7,7 @@ def list(request):
             request,
             "posts/list.html",
             {
-                "posts": Post.objects.filter(is_public=True),
+                "posts": Post.objects.public(),
             },
         )
 
