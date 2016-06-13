15a416d45d711b2048ea39a50a68e8000764eaea
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-13 21:32:12 +0900

    init comments edit, update, delete view

--- wps_blog_practice/wpsblog/wpsblog/urls/posts.py
+++ wps_blog_practice/wpsblog/wpsblog/urls/posts.py
@@ -12,5 +12,8 @@ urlpatterns = [
     url(r'^(?P<post_id>\d+)/delete/$', delete, name="delete"),
     url(r'^(?P<post_id>\d+)/$', detail, name="detail"),
 
-    url(r'^(?P<post_id>\d+)/comments/create/', comments_create, name="comments_create"),
-    ]
+    url(r'^(?P<post_id>\d+)/comments/create/$', comments_create, name="comments-create"),
+    url(r'^(?P<post_id>\d+)/comments/(?P<comment_id>\d+)/update/$', comments_update, name="comments-update"),
+    url(r'^(?P<post_id>\d+)/comments/(?P<comment_id>\d+)/edit/$', comments_edit, name="comments-edit"),
+    url(r'^(?P<post_id>\d+)/comments/(?P<comment_id>\d+)/delete/$', comments_delete, name="comments-delete"),
+]
