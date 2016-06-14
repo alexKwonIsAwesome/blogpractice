b3e6f9a3ea6a6c94d583d67c36e288bb8538fa5f
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-14 13:02:25 +0900

    init auth related views: including login, signup,

---
+++ wps_blog_practice/wpsblog/wpsblog/urls/auth.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from wpsblog.views.auth import *
+
+
+urlpatterns = [
+        url(r'^signup/$', signup, name="signup"),
+        url(r'^login/$', login, name="login"),
+        ]
+
+
+
