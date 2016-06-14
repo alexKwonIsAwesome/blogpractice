e0b0ee640952b6190994e2a4ea073f1def9179ab
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-15 00:30:30 +0900

    update Post Model: added user, objects field

--- wps_blog_practice/wpsblog/wpsblog/models/post.py
+++ wps_blog_practice/wpsblog/wpsblog/models/post.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.urlresolvers import reverse
-
+from django.contrib.auth.models import User
 
 class PostManager(models.Manager):
     
@@ -9,13 +9,22 @@ class PostManager(models.Manager):
 
 
 class Post(models.Model):
+
+    objects = PostManager()
+
+    user = models.ForeignKey(User)
+
     title = models.CharField(
             max_length=120,
         )
     content = models.TextField()
+    image = models.ImageField(
+            blank=True,
+            null=True,
+        )
     is_public = models.BooleanField(
             default=True,
-            )
+        )
 
     def __str__(self):
         return self.title
@@ -35,3 +44,9 @@ class Post(models.Model):
                     "post_id": self.id,
                     }
                 )
+
+    def get_image_url(self):
+        if self.image:
+            return self.image.url
+        return "http://placehold.it/300x200"
+
