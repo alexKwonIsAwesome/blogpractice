bf7667115f798379a2059b4fd1c2c69310328128
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-14 11:03:09 +0900

    update media seetings: added MEDIA_ROOT, add explicit MEDIA_URL

--- wps_blog_practice/wpsblog/wpsblog/settings/partials/static.py
+++ wps_blog_practice/wpsblog/wpsblog/settings/partials/static.py
@@ -1,14 +1,15 @@
 import os
 
-from .base import PROJECT_ROOT_URL
+from .base import PROJECT_ROOT_DIR
 
 
 STATIC_URL = '/static/'
 
 
 MEDIA_ROOT = os.path.join(
-        PROJECT_ROOT_URL,
+        PROJECT_ROOT_DIR,
         "dist",
         "media",
     )
+MEDIA_URL = '/media/'
 
