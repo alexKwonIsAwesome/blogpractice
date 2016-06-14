e940e5c762c932923ca3abfd2c03b6131741e323
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-14 20:41:53 +0900

    init auth views: added login and signup

--- wps_blog_practice/wpsblog/wpsblog/views/auth/login.py
+++ wps_blog_practice/wpsblog/wpsblog/views/auth/login.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.core.urlresolvers import reverse
+
 
 def login(request):
+
+    if (request.method == "POST"):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            auth_login(request, user)
+            return redirect(reverse("home"))
+        return redirect(reverse("auth:login"))
+
     return render(
             request,
             "auth/login.html",
--- wps_blog_practice/wpsblog/wpsblog/views/auth/signup.py
+++ wps_blog_practice/wpsblog/wpsblog/views/auth/signup.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+
 
 def signup(request):
+
+    if (request.method == "POST"):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        email = request.POST.get("email")
+
+        user = User.objects.create_user(
+                username = username,
+                password = password,
+                email = email,
+            )
+
+        return redirect(reverse("auth:login"))
+
     return render(
             request,
             "auth/signup.html",
