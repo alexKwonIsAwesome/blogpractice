632d7834737f4510257673fd80bc65b8c1cdecc8
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-15 10:36:00 +0900

    update posts list view and template: add pagination

--- wps_blog_practice/wpsblog/wpsblog/views/posts/list.py
+++ wps_blog_practice/wpsblog/wpsblog/views/posts/list.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from wpsblog.models import Post
 
 def list(request):
+    page = request.GET.get("page",1)
+    previous_page = int(page) - 1
+    next_page = int(page) + 1
+    per = request.GET.get("per", 5)
+
+    paginator = Paginator(Post.objects.public(), per)
+    posts = paginator.page(page)
+
     return render(
             request,
             "posts/list.html",
             {
-                "posts": Post.objects.filter(is_public=True),
+                "posts": posts,
+                "previous_page":previous_page,
+                "next_page":next_page,
             },
         )
 
