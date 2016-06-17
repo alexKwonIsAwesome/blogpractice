62449d1a00234676f4ad004df4f78e32835077f9
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-18 07:36:33 +0900

    update PostListView

--- wps_blog_practice/wpsblog/wpsblog/views/posts/list.py
+++ wps_blog_practice/wpsblog/wpsblog/views/posts/list.py
@@ -1,24 +1,10 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
-
 from wpsblog.models import Post
+from django.views.generic.list import ListView
 
-def list(request):
-    page = request.GET.get("page",1)
-    previous_page = int(page) - 1
-    next_page = int(page) + 1
-    per = request.GET.get("per", 5)
 
-    paginator = Paginator(Post.objects.public(), per)
-    posts = paginator.page(page)
+class PostListView(ListView):
 
-    return render(
-            request,
-            "posts/list.html",
-            {
-                "posts": posts,
-                "previous_page":previous_page,
-                "next_page":next_page,
-            },
-        )
+    model = Post
+    template_name = "posts/list.html"
+    context_objects_name = "posts"
 
