cd5116d2ace421ce457f7aace80ea9ad57420098
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-18 15:21:44 +0900

    update PostListView: add pagination feature

--- wps_blog_practice/wpsblog/wpsblog/views/posts/list.py
+++ wps_blog_practice/wpsblog/wpsblog/views/posts/list.py
@@ -6,3 +6,6 @@ class PostListView(PostBaseView,ListView):
 
     template_name = "posts/list.html"
     context_object_name = "posts"
+
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get("per",5)
