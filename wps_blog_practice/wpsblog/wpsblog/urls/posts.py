f0d6a28c2bd6da3c3c74652d128a3b23aeb744b6
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-18 15:01:24 +0900

    fic url reverse: change post_id to pk

--- wps_blog_practice/wpsblog/wpsblog/urls/posts.py
+++ wps_blog_practice/wpsblog/wpsblog/urls/posts.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^(?P<post_id>\d+)/edit/$', edit, name="edit"),
     url(r'^(?P<post_id>\d+)/update/$', update, name="update"),
     url(r'^(?P<post_id>\d+)/delete/$', delete, name="delete"),
-    url(r'^(?P<post_id>\d+)/$', PostDetailView.as_view(), name="detail"),
+    url(r'^(?P<pk>\d+)/$', PostDetailView.as_view(), name="detail"),
 
     url(r'^(?P<post_id>\d+)/comments/create/$', comments_create, name="comments-create"),
     url(r'^(?P<post_id>\d+)/comments/(?P<comment_id>\d+)/update/$', comments_update, name="comments-update"),
